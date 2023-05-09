446ad2dc8a3e8e72aad15048cfae527adda0b96f
Matheus de Almeida <108769812+dalmeidamatheus@users.noreply.github.com>
2023-05-08 21:25:50 -0300

    Add files via upload

--- crud.js
+++ crud.js
@@ -16,6 +16,10 @@ function cadastrar(){
     document.querySelector('#tarefas').innerHTML += gerar_card(tarefa)
 }
 
+function apagar(botao){
+  botao.parentNode.parentNode.parentNode.remove()
+}
+
 function gerar_card(tarefa){
     return `<div class="col-12 col-md-6 col-lg-3">
     <div class="card">
@@ -35,8 +39,8 @@ function gerar_card(tarefa){
         <a href="#" class="btn btn-success">
           <i class="bi bi-check2-circle"></i>
         </a>
-        <a href="#" class="btn btn-danger">
-          <i class="bi bi-trash"></i>
+        <a href="#" onClick="apagar(this)" class="btn btn-danger" title="apagar tarefa">
+                    <i class="bi bi-trash3"></i>
         </a>
       </div>
 
